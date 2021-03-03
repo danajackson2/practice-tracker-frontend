928eb813ba9715f1eff9eb6b99d85251c4501a4e
danajackson2 <danajackson2@gmail.com>
2021-03-03 11:59:01 -0800

    timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,41 +1,36 @@
 import React from 'react'
-let timer
-class Timer extends React.Component{
-    
-    state = {
-        seconds: 0
+
+function Timer(){
+
+    let timer = null
+
+    const updateTimer = () => { 
+        const currTime = document.getElementById('timer-count').textContent
+        const newSeconds = currTime.split(':')[0]*3600 + currTime.split(':')[1]*60 + currTime.split(':')[2]*1 + 1
+        const hr = ('0' + Math.floor(newSeconds/3600)).slice(-2)
+        const min = ('0' + Math.floor(newSeconds/60)).slice(-2)
+        const sec = ('0' + (newSeconds % 60)).slice(-2)
+        document.getElementById('timer-count').textContent = `${hr}:${min}:${sec}`
     }
 
-    updateTimer = () => {
-        this.setState(prevState => {
-            return {seconds: prevState.seconds + 1}
-        })
-    }
-
-    startTimer = (e) => {
-        e.stopPropagation()
-        if (!timer){ 
-            timer = setInterval(this.updateTimer, 1000)
+    const startTimer = () => {
+        if (timer === null){ 
+            return timer = setInterval(updateTimer, 1000)
         }
     }
 
-    stopTimer = (e) => {
-        e.stopPropagation()
-            clearTimeout(timer)
+    const stopTimer = () => {
+        clearInterval(timer)
+        timer = null
     }
 
-    render(){
-        let hours = ('0' + Math.floor(this.state.seconds/3600)).slice(-2)
-        let minutes = ('0' + Math.floor(this.state.seconds/60)).slice(-2)
-        let seconds = ('0' + (this.state.seconds % 60)).slice(-2)
-        return(
-            <div>
-                <h3 id='timer-count'>{`${hours}:${minutes}:${seconds}`}</h3>
-                <button type='button' className='btn btn-outline-light' id='timer-button' onClick={this.startTimer}>Start</button>
-                <button type='button' className='btn btn-outline-light' id='timer-button' onClick={this.stopTimer}>Stop</button>
-            </div>
-        )
-    }
+    return(
+        <div>
+            <h3 id='timer-count'>{'00:00:00'}</h3> 
+            <button type='button' className='btn btn-outline-light' id='timer-button' onClick={startTimer}>Start</button>
+            <button type='button' className='btn btn-outline-light' id='timer-button' onClick={stopTimer}>Stop</button>
+        </div>
+    )
 }
 
 export default Timer
--- src/components/newSessionForm/Player.js
+++ src/components/newSessionForm/Player.js
@@ -1,4 +1,5 @@
 import React from 'react'
+const BASE_URL = 'http://localhost:3000'
 
 function Player(props){
     
@@ -26,14 +27,25 @@ function Player(props){
         let seconds = ('0' + (Math.floor(timeSec) % 60)).slice(-2)
         document.getElementById(`time-left${props.data.id}`).textContent = `${minutes}:${seconds}`
     }
+
+    const deleteRecording = id => {
+        fetch(`${BASE_URL}/recordings/${id}`,{
+            method:'DELETE',
+            headers: {Authorization: `Bearer ${localStorage.token}`}
+        })
+        .then(res => res.json())
+        .then(data => console.log(data.message))
+        document.querySelector(`#audio${props.data.id}`).parentElement.remove()
+    }
     
     return (
         <div className="audio-player">
             <audio src={props.data.url} id={`audio${props.data.id}`} onEnded={endOfClip} onTimeUpdate={updateTime}/>
             <div style={{display:'flex', flexDirection:'row', justifyContent:'space-between', alignItems:'center', fontSize:'20px'}}>
                 <button className='btn btn-outline-light' id={`playAudio${props.data.id}`} onClick={e => togglePlay(e)} style={{width:'40px', height:'40px', borderRadius:'20px', margin:'0px'}}>{'>'}</button>
-                <div>{props.data.name}</div>
-                <p id={`time-left${props.data.id}`} style={{margin: '0 0 0 5px', alignSelf:'flex-start'}}>00:00</p>
+                <div>{props.data.name}<span onClick={() => window.confirm('Are you sure you want to delete this recording?') && deleteRecording(props.data.id)}> 🗑</span></div>
+                <p id={`time-left${props.data.id}`}>00:00</p>
+
             </div>
         </div>
     )
